e8da24e8461272a535a04df2158e3e8ba749b059
Denny Azevedo <denny.filho@animaeducacao.com.br>
2022-11-14 19:48:17 -0300

    Ligação com Firebase - criando as rotas

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>DashBorad</h1>
+    </div>
+  );
+}
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,9 @@
+function SignUp() {
+  return (
+    <div>
+      <h1>Tela Cadastro Usuario</h1>
+    </div>
+  );
+}
+
+export default SignUp;
