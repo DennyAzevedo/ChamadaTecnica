ca9205f80bfb84ee8b5659c315914551ce8f2495
Denny Azevedo <denny.filho@animaeducacao.com.br>
2022-11-21 20:00:05 -0300

    Cadastrando e Logando

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -21,8 +21,86 @@ function AuthProvider({ children }){
     loadStorage();
   }, [])
 
+  async function signIn(email, password){
+    setLoadingAuth(true);
+      await firebase.auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(async (value)=> {
+      let uid = value.user.uid;
+      const userProfile = await firebase
+        .firestore()
+        .collection('users')
+        .doc(uid)
+        .get();
+      let data = {
+        uid: uid,
+        nome: userProfile.data().nome,
+        avatarUrl: userProfile.data().avatarUrl,
+        email: value.user.email
+      };
+      setUser(data);
+      storageUser(data);
+      setLoadingAuth(false);
+    })
+    .catch((error)=>{
+      console.log(error);
+      setLoadingAuth(false);
+    })
+  }
+  async function signUp(email, password, nome){
+    setLoadingAuth(true);
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then( async (value)=>{
+      let uid = value.user.uid;
+      await firebase
+        .firestore()
+        .collection('users')
+        .doc(uid).set({
+          nome: nome,
+          avatarUrl: null,
+        })
+      .then( () => {
+        let data = {
+          uid: uid,
+          nome: nome,
+          email: value.user.email,
+          avatarUrl: null
+        };
+        setUser(data);
+        storageUser(data);
+        setLoadingAuth(false);
+      })
+    })
+    .catch((error)=>{
+      console.log(error);
+      setLoadingAuth(false);
+    })
+  }
+
+  function storageUser(data){
+    localStorage.setItem('SistemaUser', JSON.stringify(data));
+  }
+
+  async function signOut(){
+    await firebase.auth().signOut();
+    localStorage.removeItem('SistemaUser');
+    setUser(null);
+  }  
+
   return(
-    <AuthContext.Provider value={{ signed: !!user,  user, loading }}>
+    <AuthContext.Provider
+      value={{
+        signed: !!user,
+        user,
+        loading,
+        signUp,
+        signOut,
+        signIn,
+        loadingAuth
+      }}
+    >
       {children}
     </AuthContext.Provider>
   )
