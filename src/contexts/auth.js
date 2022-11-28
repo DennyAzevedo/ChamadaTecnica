12063d8f1572ed3c06f25e343dbd6a44e11e04db
Denny Azevedo <denny.filho@animaeducacao.com.br>
2022-11-28 19:18:50 -0300

    Criando os toosts

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,6 +1,7 @@
 
 import { useState, createContext, useEffect } from 'react';
 import firebase from '../services/firebaseConnection';
+import { toast } from 'react-toastify';
 
 export const AuthContext = createContext({});
 
@@ -40,9 +41,11 @@ function AuthProvider({ children }){
       setUser(data);
       storageUser(data);
       setLoadingAuth(false);
+      toast.success('Bem vindo de volta!');
     })
     .catch((error)=>{
       console.log(error);
+      toast.error('Ops algo deu errado!');
       setLoadingAuth(false);
     })
   }
@@ -69,10 +72,12 @@ function AuthProvider({ children }){
         setUser(data);
         storageUser(data);
         setLoadingAuth(false);
+        toast.success('Bem vindo a plataforma!');
       })
     })
     .catch((error)=>{
       console.log(error);
+      toast.error('Ops algo deu errado!');
       setLoadingAuth(false);
     })
   }
