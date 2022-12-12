a4df452e5daa7e45c0c7ac5d9dc998225e6da351
Denny Azevedo <denny.filho@animaeducacao.com.br>
2022-12-12 19:04:18 -0300

    Página Novo Chamado -  parte 3

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,13 +1,51 @@
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import firebase from '../../services/firebaseConnection';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
+import { AuthContext } from '../../contexts/auth';
 import './new.css';
 import { FiPlusCircle } from 'react-icons/fi'
 
 export default function New() {
+	const [loadCustomers, setLoadCustomers] = useState(true);
+  const [customers, setCustomers] = useState([]);
+  const [customerSelected, setCustomerSelected] = useState(0);
 	const [assunto, setAssunto] = useState('Suporte');
   const [status, setStatus] = useState('Aberto');
-  const [complemento, setComplemento] = useState('');
+	const [complemento, setComplemento] = useState('');
+	const { user } = useContext(AuthContext);
+
+	useEffect(()=> {
+    async function loadCustomers(){
+			await firebase
+				.firestore()
+				.collection('customers')
+      	.get()
+      	.then((snapshot)=>{
+        	let lista = [];
+					snapshot.forEach((doc) => {
+						lista.push({
+							id: doc.id,
+							nomeFantasia: doc.data().nomeFantasia
+						})
+					})
+					if(lista.length === 0){
+						console.log('NENHUMA EMPRESA ENCONTRADA');
+						setCustomers([ { id: '1', nomeFantasia: 'FREELA' } ]);
+						setLoadCustomers(false);
+						return;
+					}
+        	setCustomers(lista);
+        	setLoadCustomers(false);
+      	})
+				.catch((error)=>{
+					console.log('DEU ALGUM ERRO!', error);
+					setLoadCustomers(false);
+					setCustomers([ { id: '1', nomeFantasia: '' } ]);
+				})
+    }
+    loadCustomers();
+  }, []);
 
   function handleRegister(e){
     e.preventDefault();
@@ -20,6 +58,12 @@ export default function New() {
 	
 	function handleOptionChange(e){
     setStatus(e.target.value);
+	}
+	
+	function handleChangeCustomers(e){
+    //console.log('INDEX DO CLIENTE SELECIONADO: ', e.target.value);
+    //console.log('Cliente selecionado ', customers[e.target.value])
+    setCustomerSelected(e.target.value);
   }
 
   return(
@@ -31,14 +75,21 @@ export default function New() {
         </Title>
         <div className="container">
           <form className="form-profile"  onSubmit={handleRegister} >
-            <label>Cliente</label>
-            <select>
-              <option key={1} value={1}>
-                Sujeito Programador
-              </option>
-            </select>
+            {loadCustomers ? (
+              <input type="text" disabled={true} value="Carregando clientes..." />
+            ) : (
+              <select value={customerSelected} onChange={handleChangeCustomers} >
+                {customers.map((item, index) => {
+                  return(
+                    <option key={item.id} value={index} >
+                      {item.nomeFantasia}
+                    </option>
+                  )
+                })}
+              </select>
+            )}
             <label>Assunto</label>
-            <select>
+            <select value={assunto} onChange={handleChangeSelect}>
               <option value="Suporte">Suporte</option>
               <option value="Visita Tecnica">Visita Tecnica</option>
               <option value="Financeiro">Financeiro</option>
